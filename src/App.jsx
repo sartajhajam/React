136879d983100666102694719ed530fe8efc67f2
sartaj <hajamsaj@gmail.com>
2025-02-23 16:18:10 +0530

    1st components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,53 @@
-
-
 function App() {
   return (
-    <div>
-      <PostComponent />
+    <div style={{ background: "#dcdde1", height: "100vh" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>
+          <PostComponent />
+        </div>
+        <div>
+          <PostComponent />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-const style = {width:200,backgroundColor:"white",borderRadius:10,borderWidth:10,borderColor:"gray"}
+const style = {
+  width: 200,
+  backgroundColor: "white",
+  borderRadius: 10,
+  borderWidth: 1,
+  borderColor: "gray",
+  padding: 20,
+};
 
 function PostComponent() {
   return (
-    <div style={style}>
-      <img src={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg" } style={{
-        width:20,
-        height:20,
-        borderRadius:200,
-      }} />
+    <div style={{ style }}>
+      <div style={{ display: "flex" }}>
+        <img
+          src={
+            "https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
+          }
+          style={{
+            width: 30,
+            height: 30,
+            borderRadius: 20,
+          }}
+        />
+        <div style={{ fontSize: 10, marginLeft: 10 }}>
+          <b>100xdevs</b>
+          <div>23,888 followers</div>
+          <div>12 M</div>
+        </div>
+      </div>
+      <div style={{ fontSize: 10 }}>
+        What to know how to win big ? Check out these folks won 6000 $ in
+        bounties ?
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
